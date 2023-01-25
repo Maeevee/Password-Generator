2e69dc67ae9c1287880b24b193c3118bbe6e2b1f
Maeve <mariya.kuryakulova@gmail.com>
2023-01-25 19:19:39 +0000

    add-random
    Added function that randomly picks elements from arrays

--- script.js
+++ script.js
@@ -121,14 +121,14 @@ function getPasswordOptions() {
     passwordLength: passwordLength,
     userChoices: userChoices,
   };
-
 }
 
 const passwordOptions = getPasswordOptions();
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
 }
 
 // Function to generate password with user input
